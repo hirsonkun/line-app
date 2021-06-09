6004ef285c03052c08e8f1a79bb3ddc768307b0a
hirsonkun <hirsonforwork@gmail.com>
2021-06-09 22:26:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+function generate() {
+    "use strict"
+    document.getElementById("copied").innerHTML = ""
+    const wangyEl = document.getElementById("hasil");
+    const inputEl = document.getElementById("waifu");
+    const jenisEl = document.getElementsByName("jenis");
+    let jenis = ""
+    for (let i = 0, len = jenisEl.length; i < len; i++) {
+        if (jenisEl[i].checked) {
+            jenis = jenisEl[i].value
+            break;
+        }
+    }
+    let nama_waifu = inputEl.value;
+    let waifu = null
+    switch (jenis.toLowerCase()) {
+        case 'a': {
+            waifu = `${nama_waifu} ${nama_waifu} ${nama_waifu}  ❤️ ❤️ ❤️ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut ${nama_waifu} wangi aku mau nyiumin aroma wanginya ${nama_waifu} AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~ AAAAAH ${nama_waifu} keluar pertama kali di anime juga manis ❤️ ❤️ ❤️ banget AAAAAAAAH ${nama_waifu} AAAAA LUCCUUUUUUUUUUUUUUU............${nama_waifu} AAAAAAAAAAAAAAAAAAAAGH ❤️ ❤️ ❤️apa ? ${nama_waifu} itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ❤️ ❤️ ❤️ ${nama_waifu} gw ...${nama_waifu} di laptop ngeliatin gw, ${nama_waifu} .. kamu percaya sama aku ? aaaaaaaaaaah syukur ${nama_waifu} aku gak mau merelakan ${nama_waifu} aaaaaah ❤️ ❤️ ❤️ YEAAAAAAAAAAAH GUA MASIH PUNYA ${nama_waifu} SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH`
+            break;
+        }
+        case 'b': {
+            waifu = `Bro, gue gemeteran. GUE GEMETERAN ANJING Gue gak pernah mau berkembangbiak dengan siapapun lebih daripada seorang ${nama_waifu}. Badannya yang cakep, TETE GEDE, pinggul seksi dari seorang BIDADARI. Jujur aja, sakit hati kalau tau KENYATAANNYA gue GAK AKAN PERNAH BISA BUAT KAWIN SAMA DIA, ngewarisin gen gue lewat dia, dan ngebiarin dia ngelahirin anak yang sempurna dari gue.Gue rela ngelakuin APAPUN demi kesempatan buat bikin ${nama_waifu} hamil. A P A P U N. Dan gue bener-bener gk bisa terima kenyataan. Kenapa Authornya rela bikin suatu hal yang sempurna kyk dia? Buat ngegoda kita? NGETAWAIN KITA DI DEPAN MUKA?SUMPAH BRO, GUE UDAH BENER BENER GAK TAHAN. ANJING.`
+            break;
+        }
+        case 'c': {
+            waifu = `Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng ${nama_waifu}. Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot ${nama_waifu}. Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau ${nama_waifu} jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang ${nama_waifu}, dan INI MURNI PIKIRAN DAN PERASAAN GUE.`
+            break;
+        }
+        case 'd': {
+            waifu = `I would literally never stop trying to impregnate ${nama_waifu}. Every day I would wake her up by coming in her and every night I would cum in her right before going to sleep, which I would do with my dick stuck insider her. I would take some viagra before bed just to maintain my erection so that she'll be ready in the morning when I thrust into her like an animal and slather her in kisses. Part of our wedding vows would be to have as many children as physically possible. I wouldn't even care if she's already pregnant, I'll fuck her while she's pregnant and she'll get double pregnant. I'll fill her with so much cum every day that she'll look pregnant even when she isn't (which she'll never be after we're married) I would do everything in my power to make ${nama_waifu} as fertile as possible. I'd give her fertility drugs, I'd give her uterus massages, breast massages, I wouldn't let her go 12 hours without at least one spastic orgasm. I'll even bake her home made lactation inducing biscuits to help her get to a point of hyperlactation syndrome so that she'll be seeping out multiple quarts of milk per day. Which I will save and drink just so that I can tell her how delicious it is. I'll make her so fertile that triplets will be the minimum number she's carrying at any given time. I would literally never stop doting on her, I would respond to her every beck and call and I would cum inside her again each time she asks for something. She would be so pregnant all the time that she would literally not be able to stand up straight anymore even after menopause. Her spine would be permanently bent out of shape to accommodate a pregnant belly. Even after she can't get pregnant anymore I would just keep putting more eggs into her. I would clone her purely so that I can put fresh eggs from the clone inside her after she runs out of them. She would have so much progesterone running through her veins at any given time that even the thought of not being pregnant would seem alien to her.`
+            break;
+        }
+        case 'e': {
+            waifu = `NENEN NENEN KEPENGEN NENEN SAMA ${nama_waifu}. TETEK GEDE NAN KENCANG MILIK ${nama_waifu} MEMBUATKU KEPENGEN NENEN. DIBALUT PAKAIAN KETAT YANG ADUHAI CROOOOTOTOTOTOTOT ANJING SANGE GUA BANGSAT. ${nama_waifu}, PLIS DENGERIN BAIK BAIK. TOLONG BUKA BAJU SEBENTAR SAJA PLISSS TOLOOONG BANGET, BIARKAN MULUT KERINGKU BISA MENGECAP NENEN ${nama_waifu}. BIARKAN AKU MENGENYOT NENENMU ${nama_waifu}. AKU RELA NGASIH SESEMBAHAN APA AJA BERAPAPUN ITU DUIT YANG AKU BAKAR KHUSUS TERKHUSUS BUATMU. TAPI TOLOOOONG BANGET BUKA BAJUMU AKU MAU NENEN. NENEN NENEEEEN NENEN ${nama_waifu} WANGIIII`
+            break;
+        }
+        case 'f': {
+            waifu = `Sejujurnya gw ga nyangka ama tindakan lo yg ga dewasa begini Kita udh temenan dri kecil ,melalui berbagai kenangan ,tapi sikaplo begini ke gw ,ga habis pikir. Padahal sudah berjanji tidak mengusik hubungan satu sama lain lagi ,tapi maksud tindakan mu sekarang ini apa? Tiba tiba di pagi bangun tidur lu make Pp ${nama_waifu}. Lu kira lucu begitu anjing? Make waifu pp org seenaknya? Ngeklaim pula bangsad maksudnya apa apaan coba . Pertemanan dari kecil kita ga dihargai sama sekali. \nGw tunggu klarifikasi lo `
+            break;
+        }
+        case 'g': {
+            waifu = `Usiaku 22 tahun. Aku sangat mencintai ${nama_waifu}, aku punya semua Figurine dan wallpapernya. Aku berdoa setiap malam dan berterima kasih atas segala hal yang telah ia berikan kepadaku. \"${nama_waifu} adalah cinta\" aku bilang \"${nama_waifu} adalah Tujuan Hidupku\". Temanku datang ke kamarku dan berkata \"HALU LU ANJING !!\". Aku tau dia cemburu atas kesetiaanku kepada ${nama_waifu}. Lalu kukatakan padanya \"BACOT NJING !!\". Temanku menampol kepalaku dan menyuruhku untuk tidur. Kepalaku sakit dan aku menangis. Aku rebahan di kasur yang dingin, lalu ada sesuatu yang hangat menyentuhku. Ternyata ${nama_waifu} datang ke dalam kamarku, Aku begitu senang bertemu ${nama_waifu}. Dia membisikan ke telingaku, \"Kamu adalah impianku\" Dengan tangannya dia meraih diriku. Aku melebarkan pantatku keatas demi ${nama_waifu}. Dia menusukan sesuatu kedalam Anggusku. begitu sakit, tapi kulakukan itu demi ${nama_waifu}. Aku ingin memberikan kepuasan kepada ${nama_waifu}. Dia meraum bagaikan singa, disaat dia melepaskan cintanya kedalam Anggusku. Temanku masuk kekamarku dan berkata \"....... Anjing\". ${nama_waifu} melihat temanku dan berkata \" Semua sudah berakhir\" Dengan menggunakan kemampuannya Stellar Restoration ${nama_waifu} pergi meninggalkan kamarku. \"${nama_waifu} itu cinta\" \"${nama_waifu} itu kehidupan\". `
+            break;
+        }
+        default:
+            waifu = "Pilih Jenis Template"
+            /* source nya lupa dari siapa XD */
+            break;
+    }
+    nama_waifu ? wangyEl.innerHTML = waifu : wangyEl.innerHTML = "Tulis nama kamu"
+}
+function copyText() {
+    var copyText = document.getElementById("hasil");
+    if (copyText.value) {
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        document.getElementById("copied").innerHTML = "Text Berhasil Disalin"
+    }
+}
