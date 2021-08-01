cc0cb7b83082bd7d8be36e25a5f26453d9e96501
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-01 20:10:38 +0700

    Update videosline.js

--- videosline.js
+++ videosline.js
@@ -6,7 +6,7 @@ async function onGetVideos() {
         const splitHTML = html.split('background-image:url(');
         splitHTML.shift();
         const hasil = splitHTML.map((e) => e = `<video width="320" height="240" controls><source src="${e.split('/L')[0]}" type="video/mp4"></video>`).join(' ').trim();
-        document.getElementById('link_download').innerHTML = hasil || '<p>Invalid Link!</>;
+        document.getElementById('link_download').innerHTML = hasil || '<p>Invalid Link!</>';
     } catch (error) {
         document.getElementById('link_download').innerHTML = `<p>Error : ${error.message}</p>`;
     }
