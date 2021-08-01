944b842fc59fbb5370eda9f4423504059cfe4b5d
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-01 20:09:05 +0700

    Update videosline.js

--- videosline.js
+++ videosline.js
@@ -3,13 +3,10 @@ async function onGetVideos() {
     try {
         const url = "https://api.allorigins.win/raw?url=" + document.getElementById("urlVideos").value;
         const html = (await (await fetch(url)).text());
-
         const splitHTML = html.split('background-image:url(');
         splitHTML.shift();
-        const hasil = splitHTML.map((e) => {
-            `<iframe width="300" height="300" src="${e.split('/L')[0]}"></iframe>`
-        }).join(' ')
-        document.getElementById('link_download').innerHTML = hasil
+        const hasil = splitHTML.map((e) => e = `<video width="320" height="240" controls><source src="${e.split('/L')[0]}" type="video/mp4"></video>`).join(' ').trim();
+        document.getElementById('link_download').innerHTML = hasil || '<p>Invalid Link!</>;
     } catch (error) {
         document.getElementById('link_download').innerHTML = `<p>Error : ${error.message}</p>`;
     }
