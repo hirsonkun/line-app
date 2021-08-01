5a2350db52c57dde7f89d6a96ff5659d269b6dd2
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-01 19:44:09 +0700

    Create videosline.js

---
+++ videosline.js
@@ -0,0 +1,16 @@
+async function onGetVideos() {
+    document.getElementById('link_download').innerHTML = "<p>Processing....</p>"
+    try {
+        const url = document.getElementById("urlVideos").value;
+        const html = (await (await fetch(url)).text());
+
+        const splitHTML = html.split('background-image:url(');
+        splitHTML.shift();
+        const hasil = splitHTML.map((e) => {
+            `<iframe width="300" height="300" src="${e.split('/L')[0]}"></iframe>`
+        }).join(' ')
+        document.getElementById('link_download').innerHTML = hasil
+    } catch (error) {
+        document.getElementById('link_download').innerHTML = `<p>Error : ${error.message}</p>`;
+    }
+}
