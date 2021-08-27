26e1227f0e71a202ba80400d5de4fb166a950fbb
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-27 23:24:18 +0700

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -2,20 +2,21 @@ async function onGetTimeline() {
     "use strict";
     document.getElementById('link_download').innerHTML = "<p>Processing....</p>";
     try {
-        const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
-        //const url = document.getElementById("urlTimeline").value?.trim();
-        /*const enableCors = {
+        //const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
+        const url = document.getElementById("urlTimeline").value?.trim();
+        const enableCors = {
             mode: "cors",
+            referrerPolicy: 'no-referrer',
             headers: {
-                'Content-Type': "text/html; charset=UTF-8",
+                'Content-Type': 'text/html',
                 'Access-Control-Allow-Origin': "*",
-                'Access-Control-Allow-Methods': "*",
+                'Access-Control-Allow-Methods': 'GET',
                 'Access-Control-Allow-Headers': "*",
                 'Access-Control-Allow-Credentials': true
             }
-        };*/
+        };
         document.getElementById('link_download').innerHTML = `<p>Fetch ${url}...</p>`;
-        const htmlString = (await (await fetch(url)).text());
+        const htmlString = (await (await fetch(url, enableCors)).text());
         const html = new DOMParser().parseFromString(htmlString, "text/html");
         document.getElementById('link_download').innerHTML = `<p>Extract JSON from ${url}</p>`;
         const json = JSON.parse(html.getElementById("__NEXT_DATA__").textContent);
