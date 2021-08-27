bd2b3c0e5b091541e9bbf864e1da71241c532e6a
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-27 18:46:47 +0700

    Update and rename videosline.js to timeline.js

---
+++ timeline.js
@@ -0,0 +1,23 @@
+async function onGetTimeline() {
+    "use strict";
+    document.getElementById('link_download').innerHTML = "<p>Processing....</p>";
+    try {
+        const url = "https://api.allorigins.win/raw?url=" + document.getElementById("urlTimeline").value;
+        const htmlString = (await (await fetch(url)).text());
+        const html = new DOMParser().parseFromString(htmlString, "text/html");
+        const json = JSON.parse(html.getElementById("__NEXT_DATA__").textContent);
+        const feeds = Object.values(json.props?.pageProps?.initialState?.api?.feeds || {});
+        const hasil = [];
+        feeds.forEach(({ post }) => {
+            let media = post?.contents?.media;
+            Array.isArray(media) && media.forEach(({ type, resourceId }) => {
+                type == "VIDEO" ?
+                    hasil.push(`<video width="320" height="240" controls><source src="https://obs.line-scdn.net/${resourceId}" type="video/mp4"></video>`) :
+                    hasil.push(`<img src="https://obs.line-scdn.net/${resourceId}" width="320" height="240">`);
+            });
+        });
+        document.getElementById('link_download').innerHTML = hasil.join("") || '<p>Invalid Link!</p>';
+    } catch (error) {
+        document.getElementById('link_download').innerHTML = `<p>Error : ${error.message}</p>`;
+    }
+}
