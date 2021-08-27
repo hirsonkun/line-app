8c9a755a44758e3c25aecd7ca7f3055a8aeb5cb1
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-27 22:38:51 +0700

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -3,23 +3,28 @@ async function onGetTimeline() {
     document.getElementById('link_download').innerHTML = "<p>Processing....</p>";
     try {
         //const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
+        const url = document.getElementById("urlTimeline").value?.trim();
         const enableCors = {
-			mode: "cors",
-			referrerPolicy: 'no-referrer',
-			headers: {
-				'Content-Type': 'text/html',
-				'Access-Control-Allow-Origin': "*",
-				'Access-Control-Allow-Methods': 'GET',
-				'Access-Control-Allow-Headers': "*",
-				'Access-Control-Allow-Credentials': true
-			}
-		}
-        const htmlString = (await (await fetch(document.getElementById("urlTimeline").value, enableCors)).text());
+            mode: 'cors',
+            referrerPolicy: 'no-referrer',
+            headers: {
+                'Content-Type': 'text/html',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Credentials': true
+            }
+        };
+        document.getElementById('link_download').innerHTML = `<p>Fetch ${url}...</p>`;
+        const htmlString = (await (await fetch(url, enableCors)).text());
         const html = new DOMParser().parseFromString(htmlString, "text/html");
+        document.getElementById('link_download').innerHTML = `<p>Extract JSON from ${url}</p>`;
         const json = JSON.parse(html.getElementById("__NEXT_DATA__").textContent);
         const feeds = Object.values(json.props?.pageProps?.initialState?.api?.feeds || {});
-        const hasil = [];
-        feeds.forEach(({ post }) => {
+        const hasil = [], total = feeds.length;
+        document.getElementById('link_download').innerHTML = "<p>[Progress] 0% get media from JSON.</p>";
+        feeds.forEach(({ post }, i) => {
+            document.getElementById('link_download').innerHTML = `<p>[Progress] ${(100*i)/total | 0}% get media from JSON.</p>`;
             let media = post?.contents?.media;
             Array.isArray(media) && media.forEach(({ type, resourceId }) => {
                 type == "VIDEO" ?
