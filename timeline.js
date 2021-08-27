50f730e4416d18b85054495ecaceea2d2f4b5ab7
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-08-27 23:00:19 +0700

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -2,8 +2,8 @@ async function onGetTimeline() {
     "use strict";
     document.getElementById('link_download').innerHTML = "<p>Processing....</p>";
     try {
-        //const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
-        const url = document.getElementById("urlTimeline").value?.trim();
+        const url = "https://api.allorigins.win/raw?url=" + encodeURIComponent(document.getElementById("urlTimeline").value);
+        //const url = document.getElementById("urlTimeline").value?.trim();
         const enableCors = {
             mode: 'cors',
             headers: {
