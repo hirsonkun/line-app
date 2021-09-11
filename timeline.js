bd983c80f568b23792ea53938391eb44745bf887
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2021-09-11 20:10:13 +0700

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -18,7 +18,7 @@ async function onGetTimeline() {
         const htmlString = (await (await fetch(url)).text());
         const html = new DOMParser().parseFromString(htmlString, "text/html");
         document.getElementById('link_download').innerHTML = `<p>Extract JSON from ${url}</p>`;
-        const json = JSON.parse(html.getElementById("__NEXT_DATA__").textContent);
+        const json = JSON.parse(html.getElementById("__NEXT_DATA__")?.textContent || "{}");
         const feeds = Object.values(json.props?.pageProps?.initialState?.api?.feeds || {});
         const hasil = [], total = feeds.length;
         document.getElementById('link_download').innerHTML = "<p>[Progress] 0% get media from JSON.</p>";
