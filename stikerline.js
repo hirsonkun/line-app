d5e71e0e0651001c7a78f5d8b1467880dc957ab1
Ordinary Person <68461179+hirsonkun@users.noreply.github.com>
2022-04-14 04:19:02 +0700

    Update stikerline.js

--- stikerline.js
+++ stikerline.js
@@ -1,64 +1,95 @@
-const link = ['http://dl.stickershop.line.naver.jp/products/0/0/1/ID/XX/stickersXXX.zip',
-    'http://dl.stickershop.line.naver.jp/products/0/0/1/ID/XX/stickerpackXXX.zip',
-    'http://dl.shop.line.naver.jp/themeshop/v1/products/ad/63/e5/ID/1/ANDROID/theme.zip']
-function download() {
-    "use strict"
-    let url = document.getElementById("link_stiker").value, pil = document.getElementsByName('pil')
-    url = /-/.test(url) ? url : parseInt(url?.replace(/\D/g, ''))
-    for (let i = 0, len = pil.length; i < len; i++) {
-        if (pil[i].checked) {
-            pil = pil[i].value
-            break;
-        }
+"use strict";
+const link = [
+  "http://dl.stickershop.line.naver.jp/products/0/0/1/ID/XX/stickersXXX.zip",
+  "http://dl.stickershop.line.naver.jp/products/0/0/1/ID/XX/stickerpackXXX.zip",
+  "http://dl.shop.line.naver.jp/themeshop/v1/products/ad/63/e5/ID/1/ANDROID/theme.zip",
+];
+/**
+ * Ambil id stiker/tema dari URL yang diinputkan client
+ * @param {String} url - URL stiker/tema
+ * @returns {String} Mengembalikan nilai berupa ID stiker/tema
+ */
+function getIDStiker(url = "") {
+  if (/:\/\//.test(url)) {
+    url = url.split("://")[1];
+  }
+  if (url.indexOf("sticker") !== -1) {
+    return url.replace(/[^0-9]/g, "");
+  }
+  return url.split("/").filter((search) => search.indexOf("-") !== -1);
+}
+/**
+ * Mengatasi tidak dapat download file menggunakan href.
+ * Dan ubah file untuk mencegah penamaan file yang sama
+ * @param {String} url - URL Stiker/Tema
+ * @param {String} filename - Nama file
+ * @returns {Promise<String>} Mengembalikan nilai berupa raw html
+ */
+async function getFile(url = "", filename = "") {
+  try {
+    const blob = await (await fetch(url)).blob();
+    const data = URL.createObjectURL(blob);
+    let downloadFilename = filename + "-" + Math.floor(Date.now() / 1000);
+    return `<a href="${data}" download="${downloadFilename}">Download ${filename}</a>`;
+  } catch (error) {
+    return "<p>" + error.message + "</p>";
+  }
+}
+async function download() {
+  let url = document.getElementById("link_stiker").value,
+    pil = document.getElementsByName("pil");
+  url = getIDStiker(url);
+  for (let i = 0, len = pil.length; i < len; i++) {
+    if (pil[i].checked) {
+      pil = pil[i].value;
+      break;
     }
-    if (url) {
-        let str = "Stiker"
-        switch (pil) {
-            case '1': {
-                url = link[0].replace(/XXX|ID|XX/g, function (s) {
-                    return s == "XX" ? "android" : s == "ID" ? url : ""
-                })
-                break;
-            }
-            case '2': {
-                url = link[0].replace(/XXX|ID|XX/g, function (s) {
-                    return s == "XX" ? "iphone" : s == "ID" ? url : "@2x"
-                })
-                break;
-            }
-            case '3': {
-                url = link[1].replace(/XXX|ID|XX/g, function (s) {
-                    return s == "XX" ? "android" : s == "ID" ? url : ""
-                })
-                break;
-            }
-            case '4': {
-                url = link[1].replace(/XXX|ID|XX/g, function (s) {
-                    return s == "XX" ? "iphone" : s == "ID" ? url : "@2x"
-                })
-                break;
-            }
-            case '5': {
-                url = document.getElementById("link_stiker").value
-                url = url.split('product/')[1]
-                url = url?.split('/')[0]
-                if (url) url = link[2].replace(/ID/, url)
-                str = "Tema"
-                break;
-            }
-            default: {
-                url = document.getElementById("link_stiker").value
-                url = url.split('product/')
-                url = url[1] ? url[1].split('/')[0] : url[0]
-                if (/-/.test(url)) {
-                    url = link[2].replace(/ID/, url)
-                    str = "Tema"
-                } else url = link[0].replace(/XXX|ID|XX/g, function (s) {
-                    return s == "XX" ? "android" : s == "ID" ? url : ""
-                })
-                break;
-            }
+  }
+  if (url) {
+    let str = "Stiker";
+    const regex = /XXX|ID|XX/g;
+    switch (pil) {
+      case "1": {
+        url = link[0].replace(regex, function (s) {
+          return s == "XX" ? "android" : s == "ID" ? url : "";
+        });
+        break;
+      }
+      case "2": {
+        url = link[0].replace(regex, function (s) {
+          return s == "XX" ? "iphone" : s == "ID" ? url : "@2x";
+        });
+        break;
+      }
+      case "3": {
+        url = link[1].replace(regex, function (s) {
+          return s == "XX" ? "android" : s == "ID" ? url : "";
+        });
+        break;
+      }
+      case "4": {
+        url = link[1].replace(regex, function (s) {
+          return s == "XX" ? "iphone" : s == "ID" ? url : "@2x";
+        });
+        break;
+      }
+      case "5": {
+        url = link[2].replace(/ID/, url);
+        str = "Tema";
+        break;
+      }
+      default: {
+        if (/-/.test(url)) {
+          url = link[2].replace(/ID/, url);
+          str = "Tema";
+        } else {
+          url = link[0].replace(regex, function (s) {
+            return s == "XX" ? "android" : s == "ID" ? url : "";
+          });
         }
-        document.getElementById('link_download').innerHTML = `<a href="${url}" target="_blank">Download ${str}</a>`
-    } else document.getElementById('link_download').innerHTML = "<p>Invalid Link</p>"
+        break;
+      }
+    }
+    document.getElementById("link_download").innerHTML = await getFile(url, str);
+  } else document.getElementById("link_download").innerHTML = "<p>Invalid Link</p>";
 }
